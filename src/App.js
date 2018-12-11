3ea26fb0ffd88828609823e13608485a51552d6b
Chase Jensen <soccerchase@gmail.com>
2018-12-10 21:26:13 -0700

    Initialize firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Header } from './components/common';
+import firebase from 'firebase';
 
 class App extends Component {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyB0sn7mgf1LzFumYjiZ0CcdkJYX_elGeg4',
+    authDomain: 'auth-1023.firebaseapp.com',
+    databaseURL: 'https://auth-1023.firebaseio.com',
+    projectId: 'auth-1023',
+    storageBucket: 'auth-1023.appspot.com',
+    messagingSenderId: '573986736114'
+  };
+  componentWillMount() {
+    firebase.initializeApp(firebaseConfig);
+  }
   render() {
     return (
       <View>

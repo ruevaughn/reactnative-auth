49c3eb417f04c279b49b7e0b57fa591527c27abf
Chase Jensen <soccerchase@gmail.com>
2018-12-12 19:09:00 -0700

    Password field

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,20 +3,28 @@ import { Button, Card, CardSection } from './common';
 import { Input } from './common'
 
 class LoginForm extends Component {
-  state = { email: '' };
+  state = { email: '', password: '' };
 
   render() {
     return (
       <Card>
         <CardSection>
           <Input
-            placeholder="user@gmail"
+            placeholder="user@gmail.com"
             label="Email"
             value={this.state.email}
             onChangeText={email => this.setState({ email })}
           />
         </CardSection>
-        <CardSection />
+        <CardSection>
+          <Input
+            secureTextEntry={true}
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardSection>
 
         <CardSection>
           <Button>

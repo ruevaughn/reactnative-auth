069216c54bdccafcb4f827d461fb300dfdc608ce
Chase Jensen <soccerchase@gmail.com>
2018-12-15 16:18:50 -0700

    Login Form Error

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
-import { Button, Card, CardSection } from './common';
-import { Input } from './common'
+import { Text } from 'react-native';
+import firebase from 'firebase';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { email: '', password: '' };
+  state = { email: '', password: '', error: '' };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            this.setState({ error: 'Authentication Failed.' });
+          });
+      });
+  }
 
   render() {
     return (
@@ -26,8 +39,12 @@ class LoginForm extends Component {
           />
         </CardSection>
 
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Log In
           </Button>
         </CardSection>
@@ -35,5 +52,18 @@ class LoginForm extends Component {
     );
   }
 }
+
+// The bind() method creates a new function that, when called,
+// has its this keyword set to the provided value, with a given
+// sequence of arguments preceding any provided when the new
+// function is called.
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 export default LoginForm;
